f86e549607714a6ac854d3d51f5995d1afacf670
D8fJ1GuUCTPCTvR4vLcG <sysadmin@labelident.de>
2024-03-21 07:52:02 +0100

    Update removebloat.ps1

--- debloat/removebloat.ps1
+++ debloat/removebloat.ps1
@@ -468,6 +468,17 @@ ForEach ($Key in $Keys) {
 }
 
 
+#Remove Recommended section from Start Menu
+Write-Host "Stopping Cortana from being used as part of your Windows Search Function"
+$Search = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer"
+If (!(Test-Path $Search)) {
+	New-Item $Search
+}
+If (Test-Path $Search) {
+	Set-ItemProperty $Search HideRecommendedSection -Value 1
+}
+
+
 #Disables Windows Feedback Experience
 Write-Host "Disabling Windows Feedback Experience program"
 $Advertising = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo"
@@ -879,7 +890,6 @@ If ($version -like "*Windows 11*") {
 
 	## Desktop
 	REG LOAD HKLM\Default C:\Users\Default\NTUSER.DAT
-
 	# Removes Task View from the Taskbar
 	New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Value "0" -PropertyType Dword
 	# Removes Widgets from the Taskbar
@@ -890,7 +900,6 @@ If ($version -like "*Windows 11*") {
 	New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Value "0" -PropertyType Dword
 	# Removes search from the Taskbar
 	reg.exe add "HKLM\Default\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode /t REG_DWORD /d 0 /f
-
 	REG UNLOAD HKLM\Default
 
 
