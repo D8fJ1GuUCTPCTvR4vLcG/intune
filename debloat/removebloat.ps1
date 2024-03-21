9f7773158d5c63046fd4ce55174726718cc9b0c1
D8fJ1GuUCTPCTvR4vLcG <sysadmin@labelident.de>
2024-03-21 07:28:56 +0100

    Update removebloat.ps1

--- debloat/removebloat.ps1
+++ debloat/removebloat.ps1
@@ -877,6 +877,23 @@ If ($version -like "*Windows 11*") {
 	}
 
 
+	## Desktop
+	REG LOAD HKLM\Default C:\Users\Default\NTUSER.DAT
+
+	# Removes Task View from the Taskbar
+	New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Value "0" -PropertyType Dword
+	# Removes Widgets from the Taskbar
+	New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarDa" -Value "0" -PropertyType Dword
+	# Removes Chat from the Taskbar
+	New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarMn" -Value "0" -PropertyType Dword
+	# Default StartMenu alignment 0=Left
+	New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Value "0" -PropertyType Dword
+	# Removes search from the Taskbar
+	reg.exe add "HKLM\Default\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode /t REG_DWORD /d 0 /f
+
+	REG UNLOAD HKLM\Default
+
+
 	##Load the default hive from c:\users\Default\NTUSER.dat
 	reg load HKU\temphive "c:\users\default\ntuser.dat"
 	$registryPath = "registry::hku\temphive\Software\Policies\Microsoft\Windows\WindowsCopilot"
